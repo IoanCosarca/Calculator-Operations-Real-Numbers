45ea1f8e018cd99c1f9fb23d9467805daa729c37
IoanCosarca <123205755+IoanCosarca@users.noreply.github.com>
2023-04-19 12:38:24 +0300

    Update CalculatorMVC.java

--- Calculator Buzunar/src/CalculatorMVC.java
+++ Calculator Buzunar/src/CalculatorMVC.java
@@ -2,8 +2,8 @@
 public class CalculatorMVC {
 
 	public static void main(String[] args) {
-		CalculatorModel		model		= new CalculatorModel();
-		CalculatorView		view		= new CalculatorView();
+		CalculatorModel	model		= new CalculatorModel();
+		CalculatorView	view		= new CalculatorView();
 		CalculatorController controller = new CalculatorController(model, view);
 
 	}
